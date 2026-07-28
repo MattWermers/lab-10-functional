6b4f4c22705854329b619ecc4e1c7d9fa698fe67
Matthew Wermers <matthew.wermers@gmail.com>
2026-07-28 17:35:19 -0600

    Add '/db_test'

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
+import psycopg2
+import os
 from flask import Flask
 app = Flask(__name__)
+
+database_conn = os.environ.get("DATABASE_URL")
+# conn = psycopg2.connect(database_conn)
 
 @app.route('/')
 def index():
     return 'Matthew Wermers: Render flask app for Software Dev'
+
+@app.route('/db_test')
+def db_test():
+    try:
+        conn = psycopg2.connect(database_conn)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
+
+def try_function( function , data ):
+    try:
+        db_conn = psycopg2.connect(database_conn)
+        cur = db_conn.cursor()
+
+        #call function with data
+
+        return "Function processed sucessfully"
+    except Exception as e:
+        if db_conn is not None:
+            db_conn.rollback()
+        return (f"Error {e}")
+    finally:
+        if cur is not None:
+            cur.close()
+        if db_conn is not None:
+            db_conn.close()
